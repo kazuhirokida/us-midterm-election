20a019639bb8f80817d5603fa905e43b51e3ff2f
Kazuhiro Kida <55435750+kazuhirokida@users.noreply.github.com>
2022-09-23 23:04:08 +0900

    Update house_forecast_details.py

--- house_forecast_details.py
+++ house_forecast_details.py
@@ -12,6 +12,6 @@ row['date'] = pd.to_datetime(row.lastBuildDate,format='%a, %d %b %Y %H:%M:%S %z'
 
 df = pd.read_csv('https://github.com/kazuhirokida/us-midterm-election/raw/main/us_midterm_house_forecast_details.csv',index_col='date').astype(int)
 
-df = pd.concat([df,row.set_index('date').drop(['id','year','type','name','link','sort','description','copyright'],axis=1)].astype(int)).drop_duplicates()
+df = pd.concat([df,row.set_index('date').drop(['id','year','type','name','link','sort','description','copyright'],axis=1).astype(int)]).drop_duplicates()
 
 df.to_csv('us_midterm_house_forecast_details.csv')
